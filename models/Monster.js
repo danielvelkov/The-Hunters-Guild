5b9314c932fb9a3ad5514861db37ed9b73ea4187
Daniel Velkov <danielvelkov13@gmail.com>
2025-04-07 20:31:36 +0300

    feat: add monster model
    Contains basic monster details like name, health, attacks, weaknesses, etc.

---
+++ models/Monster.js
@@ -0,0 +1,45 @@
+/** Class representing base monster info. */
+class Monster {
+  /**
+   * @param {string} id Unique Monster ID
+   * @param {string} name Monster name.
+   * @param {boolean} frenzied Whether the monster has a frenzied variant.
+   * @param {boolean} tempered Whether the monster has a tempered variant.
+   * @param {boolean} arch_tempered If it Has Arch Tempered Variant (MH Wilds - TU1)
+   * @param {string[]} locales Areas it inhabits.
+   * @param {number} base_health Health at lowest possible quest rank for monster.
+   * @param {Object[]} special_attacks Monster's signature moves.
+   * @param {string} special_attacks[].name Move name.
+   * @param {string} special_attacks[].description Attack description.
+   * @param {Object[]} weaknesses Monster's weaknesses.
+   * @param {Object[]} weaknesses[].elements Monster elemental weaknesses.
+   * @param {string} weaknesses[].elements[].name Monster elemental weakness name.
+   * @param {string} weaknesses[].elements[].icon Icon path
+   * @param {Object[]} weaknesses[].ailments Monster ailment weaknesses.
+   * @param {string} weaknesses[].ailments[].name Monster ailment weakness name.
+   * @param {string} weaknesses[].ailments[].icon Icon path
+   */
+  constructor(
+    id,
+    name,
+    frenzied,
+    tempered,
+    arch_tempered,
+    locales,
+    base_health,
+    special_attacks,
+    weaknesses
+  ) {
+    this.id = id;
+    this.name = name;
+    this.frenzied = frenzied;
+    this.tempered = tempered;
+    this.arch_tempered = arch_tempered;
+    this.locales = locales;
+    this.base_health = base_health;
+    this.special_attacks = special_attacks;
+    this.weaknesses = weaknesses;
+  }
+}
+
+module.exports = Monster;
