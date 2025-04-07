1f245eb708202ebff8a439bb65cabbd024240ee2
Daniel Velkov <danielvelkov13@gmail.com>
2025-04-07 20:37:06 +0300

    feat: add methods for retrieving details related to monster and statuses
    Add method that get monsters' attacks.
    
    Add method for getting icons and names for available statuses. (Thunder, Stamina, Poison, Sleep).
    Includes icons for the elements and other stuff too

--- db/queries.js
+++ db/queries.js
@@ -1,4 +1,23 @@
-const getMonstersAndWeaknesses = `SELECT
+const pool = require('./pool');
+
+async function getMonstersInfo__AllWeaknesses() {
+  const { rows } = await pool.query(getMonsters__AllWeaknesses);
+  return rows;
+}
+
+async function getStatusIcons__NamesAndIconId() {
+  const { rows } = await pool.query(`SELECT id, name from status_icons;`);
+  return rows;
+}
+
+async function getMonsterSpecialAttacks__NamesAndDescription() {
+  const { rows } = await pool.query(
+    `SELECT name, description from monster_special_attacks;`
+  );
+  return rows;
+}
+
+const getMonsters__AllWeaknesses = `SELECT
   em_id,
   large_monster_icon_id,
   name,
@@ -6,10 +25,12 @@ const getMonstersAndWeaknesses = `SELECT
   tempered,
   arch_tempered,
   locale,
+  base_health,
+  special_attacks,
   CONCAT(
     weakness,
     COALESCE(
-      ', ' || NULLIF(
+      '; ' || NULLIF(
         (SELECT STRING_AGG(upper(key), ', ')
          FROM (
            SELECT (jsonb_each_text).key AS key, (jsonb_each_text).value AS value
@@ -18,7 +39,7 @@ const getMonstersAndWeaknesses = `SELECT
              FROM badcondition2 bc2
              WHERE bc2.name = m.name
            ) subquery
-           WHERE (jsonb_each_text).value = '⭐⭐⭐'
+           WHERE (jsonb_each_text).value Like '⭐⭐%'
          ) inner_query
         ),
         ''
@@ -26,4 +47,24 @@ const getMonstersAndWeaknesses = `SELECT
       ''
     )
   ) AS all_weaknesses
-FROM monsters m;`;
+FROM monsters m where m.class = 'Large';`;
+
+const getMonsters__OnlyElementalWeaknesses = `SELECT
+  em_id,
+  large_monster_icon_id,
+  name,
+  frenzied,
+  tempered,
+  arch_tempered,
+  locale,
+  base_health,
+  special_attacks,
+  weakness,
+FROM monsters;
+`;
+
+module.exports = {
+  getMonstersInfo__AllWeaknesses,
+  getStatusIcons__NamesAndIconId,
+  getMonsterSpecialAttacks__NamesAndDescription,
+};
