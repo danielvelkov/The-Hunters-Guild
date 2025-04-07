4c7388ef0687c3fd08bf8c87bb2278bb4e743dd1
Daniel Velkov <danielvelkov13@gmail.com>
2025-04-07 23:34:53 +0300

    feat: add new quest post button to 'hunting quests' index page
    Set 'views' path and the view engine for the express app

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/', (req, res) => res.send('Hello Hunter!'));
 
