c903133c1821f12a71b811f7ead7c1b787de9685
Daniel Velkov <danielvelkov13@gmail.com>
2025-04-15 20:54:30 +0300

    feat: create model called 'Item'
    Contains fields for id, name, icon, iconColor etc.
    
    Icon color is the in-game color of that particular icon for that item
    
    Has prop source for where the item comes from

---
+++ models/Item.js
@@ -0,0 +1,25 @@
+/** Class representing item info. */
+class Item {
+  /**
+   * @param {string} id Unique Item ID
+   * @param {string} name Item name.
+   * @param {string} icon Icon name.
+   * @param {string} iconColor Icon coloring.
+   * @param {string} description
+   * @param {string} type Item type ('Food Ingredient', 'Material', 'Consumable', etc)
+   * @param {number} rarity 1-7 with 1 being common; 7 - Super Rare
+   * @param {string} source Where to get the item.
+   */
+  constructor(id, name, icon, iconColor, description, type, rarity, source) {
+    this.id = id;
+    this.name = name;
+    this.icon = icon;
+    this.iconColor = iconColor;
+    this.description = description;
+    this.type = type;
+    this.rarity = rarity;
+    this.source= source;
+  }
+}
+
+module.exports = Item;
