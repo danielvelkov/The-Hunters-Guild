1e0b8ef2fddd5404abded62efa2458509294dfb0
Daniel Velkov <danielvelkov13@gmail.com>
2025-05-17 21:47:57 +0300

    feat: add some static fields to weapon types and attr
    Easily create the weapon types and attr from db. Temporary, will be changed

--- models/WeaponAttribute.js
+++ models/WeaponAttribute.js
@@ -10,6 +10,17 @@ class WeaponAttribute {
     this.name = name;
     this.icon = icon;
   }
+  // TODO: (change it to init from db somehow)
+  // Static fields for each weapon attribute 
+  static FIRE = new WeaponAttribute('1', 'FIRE', 'STATUS_0000');
+  static WATER = new WeaponAttribute('2', 'WATER', 'STATUS_0001');
+  static ICE = new WeaponAttribute('3', 'ICE', 'STATUS_0003');
+  static THUNDER = new WeaponAttribute('4', 'THUNDER', 'STATUS_0002');
+  static DRAGON = new WeaponAttribute('5', 'DRAGON', 'STATUS_0004');
+  static POISON = new WeaponAttribute('6', 'POISON', 'STATUS_0005');
+  static PARALYSIS = new WeaponAttribute('7', 'PARALYSIS', 'STATUS_0007');
+  static SLEEP = new WeaponAttribute('8', 'SLEEP', 'STATUS_0009');
+  static BLAST = new WeaponAttribute('9', 'BLAST', 'STATUS_0010');
 }
 
 module.exports = WeaponAttribute;
--- models/WeaponType.js
+++ models/WeaponType.js
@@ -8,6 +8,22 @@ class WeaponType {
     this.id = id;
     this.name = name;
   }
+  // TODO: (change it to init from db somehow)
+  // Static fields for each weapon type
+  static GREAT_SWORD = new WeaponType('14', 'Great Sword');
+  static SWORD_AND_SHIELD = new WeaponType('13', 'Sword & Shield');
+  static DUAL_BLADES = new WeaponType('12', 'Dual Blades');
+  static LONG_SWORD = new WeaponType('11', 'Long Sword');
+  static HAMMER = new WeaponType('10', 'Hammer');
+  static HUNTING_HORN = new WeaponType('9', 'Hunting Horn');
+  static LANCE = new WeaponType('8', 'Lance');
+  static GUNLANCE = new WeaponType('7', 'Gunlance');
+  static SWITCH_AXE = new WeaponType('6', 'Switch Axe');
+  static CHARGE_BLADE = new WeaponType('5', 'Charge Blade');
+  static INSECT_GLAIVE = new WeaponType('4', 'Insect Glaive');
+  static BOW = new WeaponType('3', 'Bow');
+  static HEAVY_BOWGUN = new WeaponType('2', 'Heavy Bowgun');
+  static LIGHT_BOWGUN = new WeaponType('1', 'Light Bowgun');
 }
 
 module.exports = WeaponType;
