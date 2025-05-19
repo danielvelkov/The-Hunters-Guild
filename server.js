a8e0ee7c2ff2163e6d52722ce08a62fe1044661d
Daniel Velkov <danielvelkov13@gmail.com>
2025-05-19 12:16:37 +0300

    nit: server js app name change

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const app = require('./app');
 const { PORT } = process.env;
 
 app.listen(PORT, () => {
-  console.log('Hunters guild listening on port:' + PORT);
+  console.log('Hunters Guild app listening on port:' + PORT);
 });
