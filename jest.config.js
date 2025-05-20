d507ec2218ba446a7461dbd7a69e60e5387be9e5
Daniel Velkov <danielvelkov13@gmail.com>
2025-05-21 01:18:23 +0300

    build: add jest config
    Set up to use jsdom as env.
    Add a sample test for checking.

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+const config = {
+  // Automatically clear mock calls, instances, contexts and results before every test
+  clearMocks: true,
+
+  // The test environment that will be used for testing
+  testEnvironment: 'jsdom',
+
+  // The glob patterns Jest uses to detect test files
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
+};
+
+module.exports = config;
