23e18fe9eb7656ca04af55ce51d0fa50ec86c1d4
Daniel Velkov <danielvelkov13@gmail.com>
2025-05-20 10:11:42 +0300

    nit: add some comments and change .env example

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true })); // parse html form data
 
 app.use('/', huntingQuestsRouter);
 
