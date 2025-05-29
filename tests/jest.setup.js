7c31790fcf5e9bdab1e46d2d0f2ed8ed4dfb48f2
Daniel Velkov <danielvelkov13@gmail.com>
2025-05-29 18:36:32 +0300

    test: add jquery and its plugins and widgets to jest setup

--- tests/jest.setup.js
+++ tests/jest.setup.js
@@ -1,4 +1,21 @@
-import { TextEncoder, TextDecoder } from 'util';
+const { TextEncoder, TextDecoder } = require('util');
 
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
+const jQuery = require('jquery');
+
+global.$ = global.jQuery = jQuery;
+
+require('jquery-ui/ui/version');
+require('jquery-ui/ui/plugin');
+require('jquery-ui/ui/widget');
+
+const accordion = require('jquery-ui/ui/widgets/accordion');
+const tabs = require('jquery-ui/ui/widgets/tabs');
+const tooltip = require('jquery-ui/ui/widgets/tooltip');
+
+const select2 = require('select2');
+
+if (typeof $.fn.select2 === 'undefined') {
+  $.fn.select2 = select2;
+}
