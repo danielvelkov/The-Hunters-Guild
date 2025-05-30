eba8b904f6fee1d72e746dfd8896312c55517adf
Daniel Velkov <danielvelkov13@gmail.com>
2025-05-30 17:24:57 +0300

    refactor: add Slot class to entities

---
+++ src/entities/Slot.js
@@ -0,0 +1,52 @@
+import { guidGenerator } from 'js/common/common.js';
+
+export default class Slot {
+  /**
+   * @param {string} displayName - The display name for this slot (e.g., "Player 1", "Newbie Hunter").
+   * @param {boolean} [isOwner=false] - Flag indicating if this slot is for the quest owner.
+   * @param {boolean} canEdit - Flag indicating if this slot can be edited.
+   */
+  constructor({
+    id = guidGenerator().substring(0, 4),
+    displayName,
+    isOwner = false,
+    configurationType = 'Flexible',
+    loadoutName = 'Flexible Loadout',
+    loadoutDescription = 'Any skills and equipment are permitted for this slot.',
+    roles = ['ANY'],
+    weaponTypes = ['ANY'],
+    weaponAttributes = ['ANY'],
+    skills = ['ANY'],
+    monsterPartFocus = ['ANY'],
+    roleNotes = '',
+  }) {
+    // Generate ID
+    this.id = id;
+    // --- Slot Identification ---
+    this.displayName = displayName;
+    this.isOwner = isOwner;
+
+    // --- Configuration Type ---
+    this.configurationType = configurationType;
+
+    // General Loadout Information
+    this.loadoutName = loadoutName;
+    this.loadoutDescription = loadoutDescription;
+
+    this.roles = roles;
+    this.weaponTypes = weaponTypes;
+    this.weaponAttributes = weaponAttributes;
+    this.skills = skills;
+    this.monsterPartFocus = monsterPartFocus;
+    this.roleNotes = roleNotes;
+  }
+
+  initFromLoadout(loadout) {
+    this.loadoutName = loadout.name;
+    this.loadoutDescription = loadout.description;
+    this.roles = loadout.roles.map((r) => r.name);
+    this.weaponTypes = loadout.weapon_types.map((wp) => wp.id.toString());
+    this.weaponAttributes = loadout.weapon_attr.map((wa) => wa.id.toString());
+    this.skills = loadout.skills;
+  }
+}
--- src/public/js/hunting-quest/create/player-comp.js
+++ src/public/js/hunting-quest/create/player-comp.js
@@ -4,8 +4,9 @@ import {
   weaponTypesList,
   systemLoadoutsList,
 } from '../create.js';
-import { guidGenerator } from '../../common/common.js';
 import 'css/components/hunter-slot.css';
+
+import Slot from 'entities/Slot.js';
 
 class PlayerComp {
   nextSkillIndex = 0;
@@ -803,58 +804,7 @@ const getWeaponType = (id) => weaponTypesList.find((w) => w.id === id);
 const getWeaponAttribute = (id) =>
   weaponAttributesList.find((w) => w.id === id);
 
-class Slot {
-  /**
-   * @param {string} displayName - The display name for this slot (e.g., "Player 1", "Newbie Hunter").
-   * @param {boolean} [isOwner=false] - Flag indicating if this slot is for the quest owner.
-   * @param {boolean} canEdit - Flag indicating if this slot can be edited.
-   */
-  constructor({
-    id = guidGenerator().substring(0, 4),
-    displayName,
-    isOwner = false,
-    configurationType = 'Flexible',
-    loadoutName = 'Flexible Loadout',
-    loadoutDescription = 'Any skills and equipment are permitted for this slot.',
-    roles = ['ANY'],
-    weaponTypes = ['ANY'],
-    weaponAttributes = ['ANY'],
-    skills = ['ANY'],
-    monsterPartFocus = ['ANY'],
-    roleNotes = '',
-  }) {
-    // Generate ID
-    this.id = id;
-    // --- Slot Identification ---
-    this.displayName = displayName;
-    this.isOwner = isOwner;
-
-    // --- Configuration Type ---
-    this.configurationType = configurationType;
-
-    // General Loadout Information
-    this.loadoutName = loadoutName;
-    this.loadoutDescription = loadoutDescription;
-
-    this.roles = roles;
-    this.weaponTypes = weaponTypes;
-    this.weaponAttributes = weaponAttributes;
-    this.skills = skills;
-    this.monsterPartFocus = monsterPartFocus;
-    this.roleNotes = roleNotes;
-  }
-
-  initFromLoadout(loadout) {
-    this.loadoutName = loadout.name;
-    this.loadoutDescription = loadout.description;
-    this.roles = loadout.roles.map((r) => r.name);
-    this.weaponTypes = loadout.weapon_types.map((wp) => wp.id.toString());
-    this.weaponAttributes = loadout.weapon_attr.map((wa) => wa.id.toString());
-    this.skills = loadout.skills;
-  }
-}
-
 // Initialize the component
-$(document).ready(() => {
+$(() => {
   new PlayerComp();
 });
