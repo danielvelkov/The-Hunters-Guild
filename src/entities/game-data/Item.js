b991dd80474ba674e32a3323085bd92fe027f5d0
Daniel Velkov <danielvelkov13@gmail.com>
2025-06-02 21:32:19 +0300

    docs: remove item js doc for properties. did nothing

--- src/entities/game-data/Item.js
+++ src/entities/game-data/Item.js
@@ -1,14 +1,3 @@
-/**
- * Class representing item info.
- * @property {string} id - Unique Item ID
- * @property {string} name - Item name
- * @property {string} icon - Icon name
- * @property {string} iconColor - Icon coloring
- * @property {string} description - Item description
- * @property {string} type - Item type ('Food Ingredient', 'Material', 'Consumable', etc)
- * @property {number} rarity - 1-7 with 1 being common; 7 - Super Rare
- * @property {string} source - Where to get the item
- */
 class Item {
   /**
    * @param {string} id Unique Item ID
