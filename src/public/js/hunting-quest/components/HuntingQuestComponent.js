3e305ddeb014f330e9b3eb88c12daa0deed95412
Daniel Velkov <danielvelkov13@gmail.com>
2025-06-04 16:41:29 +0300

    feat: add quest details tab implementation without bonus rewards and recommended skills

--- src/public/js/hunting-quest/components/HuntingQuestComponent.js
+++ src/public/js/hunting-quest/components/HuntingQuestComponent.js
@@ -1,6 +1,9 @@
 import { bonusQuestRewardsList, monstersDropsList } from '../create.js';
+import { getDmgColor, getQuestCategoryStyle } from '../../common/common.js';
 import { guidGenerator } from 'js/common/common.js';
 import HuntingQuest from 'entities/HuntingQuest';
+import MonsterCrown from 'entities/game-data/MonsterCrown.js';
+import MonsterVariant from 'entities/game-data/MonsterVariant.js';
 
 class HuntingQuestComponent {
   /**
@@ -25,28 +28,27 @@ class HuntingQuestComponent {
     const monsterTabsIds = [
       ...this.quest.quest_monsters.map((_) => guidGenerator()),
     ];
-
     // Generate HTML for preview
     const HTMLstring = `
-    <div class="tabs">
-      <ul>
-        <li><a href="#tabs-quest-details-${questDetailsTabId}">Quest Details</a></li>
+    <div class="tabs" >
+      <ul aria-label="quest tabs">
+        <li aria-controls="tabs-quest-details-${questDetailsTabId}"><a href="#tabs-quest-details-${questDetailsTabId}">Quest Details</a></li>
         ${this.quest.quest_monsters
           .map((qm) => qm.monster)
           .map(
             (m, i) =>
-              `<li><a href="#tabs-monster-${monsterTabsIds[i]}">${m.name}</a></li>`
+              `<li aria-controls="tabs-monster-${monsterTabsIds[i]}"><a href="#tabs-monster-${monsterTabsIds[i]}">${m.name}</a></li>`
           )
           .join('')}
       </ul>
-      <div id="tabs-quest-details-${questDetailsTabId}">
+      <div role="tabpanel" id="tabs-quest-details-${questDetailsTabId}">
         ${this.generateQuestDetailsTab()}
       </div>
       ${this.quest.quest_monsters
         .map((qm) => qm.m)
         .map(
           (m, i) => `
-        <div id="tabs-monster-${monsterTabsIds[i]}">
+        <div role="tabpanel" id="tabs-monster-${monsterTabsIds[i]}">
           ${this.generateMonsterTab(m, isHighRankQuest)}
         </div>
       `
@@ -62,11 +64,127 @@ class HuntingQuestComponent {
   }
 
   generateQuestDetailsTab() {
-    //TODO
+    return `
+    <table class="themetable" width="100%" align="center">
+      <tbody>
+        <tr>
+          <th colspan="2" style="${getQuestCategoryStyle(
+            this.quest.category.name
+          )}">${this.quest.category.name}</th>
+          <th colspan="3" align="right">${this.quest.star_rank}${
+      this.quest.star_rank > 1
+        ? [...new Array(+this.quest.star_rank)].map((star) => '⭐').join('')
+        : '⭐'
+    }</th>
+        </tr>
+        <tr>
+          <th rowspan="2">Monsters:</th>
+          <th colspan="3" >
+            <div class="flex-row" style="justify-content:center;">
+            ${this.quest.quest_monsters
+              .map(
+                (qm, index) =>
+                  `<div class="flex-col" style="position:relative;">
+                ${
+                  qm.crown.name !== MonsterCrown.BASE.name
+                    ? `<img height='25' title="${qm.crown.name}" alt="${
+                        qm.crown.name
+                      }"
+                src="icons/${qm.crown.name.toLowerCase()}-crown.png"
+                style="position:absolute; z-index: 1; top:0px; right:-10px;filter: drop-shadow(1px 1px 1px #222)
+                drop-shadow(-1px -1px 1px #222) drop-shadow(-1px 1px 0px #222) drop-shadow(1px -1px 0px #222);">`
+                    : ''
+                }
+
+                <img height='75' title="${
+                  qm.variant.name !== MonsterVariant.BASE.name
+                    ? qm.variant.name + ' ' + qm.monster.name
+                    : qm.monster.name
+                }" alt="${
+                    qm.variant.name !== MonsterVariant.BASE.name
+                      ? qm.variant.name + ' ' + qm.monster.name
+                      : qm.monster.name
+                  }"
+                  class="monster-image
+                  ${
+                    qm.variant.name === MonsterVariant.TEMPERED.name
+                      ? 'tempered-outline'
+                      : ''
+                  }
+                  ${
+                    qm.variant.name === MonsterVariant.FRENZIED.name
+                      ? 'frenzied-outline'
+                      : ''
+                  }"
+                  src="icons/Large Monster Icons/${qm.monster.icon}.png"/>
+                  <span style="margin-top:-1em; font-size:0.65em;">${'✨'.repeat(
+                    +qm.strength
+                  )}</span>
+                </div>`
+              )
+              .join('')}
+            </div>
+          </th>
+        </tr>
+        <tr>
+          <th colspan="3"><h3>${this.quest.title}</h3></th>
+        </tr>
+        <tr>
+          <th>Bonus Rewards:</th>
+          <td colspan="3">
+            <div class='flex-row' style="gap:0em; align-items:center;">
+              ${this.generateBonusRewards(this.quest.quest_bonus_rewards)}
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <th width="10%"></th>
+          <th width="20%">Quest Info:</th>
+          <th width="70%" align="left">Recom. Skills:</th>
+        </tr>
+        <tr>
+          <th>HR req:</th>
+          <td>${this.quest.hr_requirement}+</td>
+          <td rowspan="3">
+            ${this.generateRecommendedSkills(
+              this.quest.quest_monsters.map((qm) => qm.monster)
+            )}
+          </td>
+        </tr>
+        <tr>
+          <th>Area:</th>
+          <td>${this.quest.area}</td>
+        </tr>
+        <tr>
+          <th>Time Limit:</th>
+          <td>${this.quest.time_limit} mins</td>
+        </tr>
+        <tr>
+          <th>Description:</th>
+          <td colspan="3"></td>
+        </tr>
+        <tr>
+          <th>Notes:</th>
+          <td colspan="3"></td>
+        </tr>
+      </tbody>
+    </table>
+  `;
   }
 
   generateMonsterTab() {
     //TODO
+    return '';
+  }
+
+  generateBonusRewards() {
+    //TODO
+    return '';
+  }
+
+  generateRecommendedSkills() {
+    //TODO
+    return '';
   }
 }
 
