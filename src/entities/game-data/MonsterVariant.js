8a69dc1472fa4fa68370d0d3456cf86c37d7308a
Daniel Velkov <danielvelkov13@gmail.com>
2025-06-04 12:36:39 +0300

    feat: add monster variant enum entity
    override the toString to return the name of the variation

---
+++ src/entities/game-data/MonsterVariant.js
@@ -0,0 +1,28 @@
+/** Enum representing different monster variants */
+class MonsterVariant {
+  /**
+   * @param {number} id Variant ID
+   * @param {string} name Variant name
+   */
+  constructor(id, name) {
+    this.id = id;
+    this.name = name;
+  }
+
+  static BASE = new MonsterVariant(1, 'Base');
+  static FRENZIED = new MonsterVariant(2, 'Frenzied');
+  static TEMPERED = new MonsterVariant(3, 'Tempered');
+  static ARCH_TEMPERED = new MonsterVariant(4, 'Arch Tempered');
+
+  static values() {
+    return Object.values(MonsterVariant).filter(
+      (v) => v instanceof MonsterVariant
+    );
+  }
+
+  toString() {
+    return this.name;
+  }
+}
+
+module.exports = MonsterVariant;
