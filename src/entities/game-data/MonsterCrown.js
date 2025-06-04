b81f8c92d28e7912068fd797e5436869517c9ecd
Daniel Velkov <danielvelkov13@gmail.com>
2025-06-04 12:36:11 +0300

    feat: add monster crown enum entity
    override the toString to return the name of the crown

---
+++ src/entities/game-data/MonsterCrown.js
@@ -0,0 +1,26 @@
+/** Enum representing Different Monster Crowns*/
+class MonsterCrown {
+  /**
+   * @param {number} id Crown ID
+   * @param {string} name Crown name
+   */
+  constructor(id, name) {
+    this.id = id;
+    this.name = name;
+  }
+
+  static BASE = new MonsterCrown(1, 'Base');
+  static MINI = new MonsterCrown(2, 'Mini');
+  static SILVER = new MonsterCrown(3, 'Silver');
+  static GOLD = new MonsterCrown(4, 'Gold');
+
+  static values() {
+    return Object.values(MonsterCrown).filter((v) => v instanceof MonsterCrown);
+  }
+
+  toString() {
+    return this.name;
+  }
+}
+
+module.exports = MonsterCrown;
