70a037ca317ef17cb9ab3a5046a2b2e706933f2f
Daniel Velkov <danielvelkov13@gmail.com>
2025-06-04 12:35:10 +0300

    feat: add hunting quest entity for representing bonus quest rewards
    Accepts only object of class 'Item' and quantity at a specified limit.

---
+++ src/entities/QuestBonusRewards.js
@@ -0,0 +1,40 @@
+const Item = require('./game-data/Item');
+
+class QuestBonusReward {
+  #item;
+  #quantity;
+  /**
+   * @param {Item} item - Game Item
+   * @param {number} quantity - Min 1, Max IDK (20 for now)
+   */
+  constructor(item, quantity = 1) {
+    this.item = item;
+    this.quantity = quantity;
+  }
+
+  get item() {
+    return this.#item;
+  }
+
+  set item(value) {
+    if (!(value instanceof Item)) {
+      throw new Error(`Invalid item: ${value}. Must be an instance of Item.`);
+    }
+    this.#item = value;
+  }
+
+  get quantity() {
+    return this.#quantity;
+  }
+
+  set quantity(value) {
+    if (typeof value !== 'number' || value < 1 || value > 20) {
+      throw new Error(
+        `Invalid quantity: ${value}. Must be a number between 1 and 20.`
+      );
+    }
+    this.#quantity = value;
+  }
+}
+
+module.exports = QuestBonusReward;
