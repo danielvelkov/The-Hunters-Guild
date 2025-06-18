87de3b815a543a8f30528917eb45c7c0ea3f2e93
Daniel Velkov <danielvelkov13@gmail.com>
2025-06-18 18:09:08 +0300

    feat: add model hunting quests to index page
    Each hunting quest passed from controller is used for the component class.
    
    Add server data from router render method to ejs template as a global variable.
    
    Include header partial view for css of jquery widgets to work.

--- src/public/js/hunting-quest/index.js
+++ src/public/js/hunting-quest/index.js
@@ -1,1 +1,19 @@
 import 'css/pages/hunting-quest/index.css';
+import HuntingQuestComponent from './components/HuntingQuestComponent';
+
+const {
+  huntingQuests,
+  monstersDropsList,
+  weaponTypesList,
+  weaponAttributesList,
+} = globalThis.serverData;
+
+const huntingQuestComponents = huntingQuests.map(
+  (hq) => new HuntingQuestComponent(hq, monstersDropsList)
+);
+
+$('ul.hunting-quests').append(
+  huntingQuestComponents.map((hqc) =>
+    $('<li>').addClass('hunting-quest-post').append(hqc.render())
+  )
+);
