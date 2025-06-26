caa20b16f21f53280a831e2af1150eadb5956771
Daniel Velkov <danielvelkov13@gmail.com>
2025-06-26 16:38:33 +0300

    feat: add schema.sql file
    File will contain queries for creating the app db tables and relationships

---
+++ db/schema.sql
@@ -0,0 +1,79 @@
+-- ENUMS ( won't change ever )
+CREATE TYPE star_rank_enum AS ENUM ('LOW', 'HIGH', '1', '2', '3', '4', '5', '6', '7', '8', '9');
+
+CREATE TYPE locale_enum AS ENUM (
+  'Iceshard Cliffs',
+  'Ruins of Wyveria',
+  'Wounded Hollow',
+  'Rimechain Peak',
+  'Dragontorch Shrine',
+  'Training Area',
+  'Scarlet Forest',
+  'Oilwell Basin',
+  'Windward Plains',
+  'Cavern Tunnel'
+);
+
+CREATE TYPE slot_config_type_enum AS ENUM ('Flexible', 'Custom', 'Preset');
+
+-- FOR HUNTING QUEST TABLE
+
+CREATE TABLE quest_categories (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+
+INSERT INTO quest_categories VALUES 
+    (1, 'Assignment'),
+    (2, 'Optional'),
+    (3, 'Event'),
+    (4, 'Field Survey'),
+    (5, 'Saved Investigation'),
+    (6, 'Arena Quest'),
+    (7, 'Challenge Quest'),
+    (8, 'Free Challenge Quest');
+
+CREATE TABLE quest_types (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+
+INSERT INTO quest_types VALUES
+    (1, 'Hunt'),
+    (2, 'Slay'),
+    (3, 'Capture'),
+    (4, 'Repel'),
+    (5, 'Other');
+
+CREATE TABLE monster_variants (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+);
+
+INSERT INTO monster_variants VALUES
+    (1, 'Base'),
+    (2, 'Frenzied'),
+    (3, 'Tempered'),
+    (4, 'Arch Tempered');
+
+CREATE TABLE gaming_platforms (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+);
+
+INSERT INTO gaming_platforms VALUES
+    (1, 'PC'),
+    (2, 'Xbox'),
+    (3, 'PlayStation');
+
+CREATE TABLE monster_crowns (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+);
+
+INSERT INTO monster_crowns VALUES
+    (1, 'Base'),
+    (2, 'Mini'),
+    (3, 'Silver'),
+    (4, 'Gold');
+
